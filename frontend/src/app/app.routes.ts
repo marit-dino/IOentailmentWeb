bcfb04d21a450effba93297d403384b6b5857d65
marit <marit@einboeck.com>
2024-03-26 10:07:21 +0100

    logo and server not reachable error message

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {EntailmentComponent} from "./entailment/entailment.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',
+  title: 'I/O Entailment',
+  component: EntailmentComponent}
+];
